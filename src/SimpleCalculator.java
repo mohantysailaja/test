740cd2f58b290ce6821428dcd0dfb7ae42c097fd
sailaja <mohantysailaja@gmail.com>
2023-04-24 11:48:48 +0100

    Simple calculator class to write unit test method in Junit

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,5 +1,8 @@
 public class SimpleCalculator {
     public int add(int numberA,int numberB){
+       // return numberA * numberB;//testing fail test case by changing
         return numberA + numberB;
+      //  return numberA - numberB;//if I change subtract and see the test case wrong//Expected:4//Actual :0
+
     }
 }
