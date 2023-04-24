56ff9c43906c7a4ba79d0dde78323479f70c7cbe
sailaja <mohantysailaja@gmail.com>
2023-04-24 11:38:41 +0100

    Simple calculator class to write unit test method in Junit

--- src/SimpleCalculatorTest.java
+++ src/SimpleCalculatorTest.java
@@ -16,3 +16,5 @@ class SimpleCalculatorTest {
 
 //https://www.youtube.com/watch?v=o5pE7L2tVV8
 //https://www.youtube.com/watch?v=vZm0lHciFsQ
+//ctrl+shift+T(while adding junit library fixing)
+//Alt+Enter,the error arises ,
