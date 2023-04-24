2c239c957c665411f1e1bcefaf918d67d070eba8
sailaja <mohantysailaja@gmail.com>
2023-04-24 11:36:16 +0100

    Simple calculator class to write unit test method in Junit

--- src/SimpleCalculatorTest.java
+++ src/SimpleCalculatorTest.java
@@ -1,4 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SimpleCalculatorTest {
-  
+
+@Test
+
+    void twoPlusTwoShouldEqualFour(){
+    SimpleCalculator calculator = new SimpleCalculator();
+    assertEquals(4,calculator.add(2,2));
 }
+}
+
+
+
+//https://www.youtube.com/watch?v=o5pE7L2tVV8
+//https://www.youtube.com/watch?v=vZm0lHciFsQ
